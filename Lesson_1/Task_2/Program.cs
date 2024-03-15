1178d047be281a62499137e78a1c63d1f6818644
Alexandra <atoxtaeva@gmail.com>
2024-03-15 13:15:04 +0300

    lesson1 task2

---
+++ Lesson_1/Task_2/Program.cs
@@ -0,0 +1,25 @@
+﻿int i1 =5;
+int i2 = 6;
+int sum = i1 + i2;
+System.Console.WriteLine(sum);
+
+// sum - это бинарная операция, где участие приниамют два операнда, соответственно результату сложения присваиваем тип данных как у операндов.
+// ctrl+S - сохраняет введенные данные кода
+
+long l1 = 6;
+long sum2 = l1 + i1;
+System.Console.WriteLine(sum2);
+
+// переменной ыгь2 присвоим тип данных long , следуя правилу:
+// если типы данных у операндов разные, то выбираем тот , что объемнее и вместительнее.
+
+double d1 = 5.5;
+double sum3 = d1 + i1;
+System.Console.WriteLine(sum3);
+
+int product1 = i1 * i2;
+long product2 = l1 * i1;
+double product3 = i1 * d1;
+System.Console.WriteLine(product1);
+System.Console.WriteLine(product2);
+System.Console.WriteLine(product3);
