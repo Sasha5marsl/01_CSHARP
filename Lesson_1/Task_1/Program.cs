b9c0bb7327daa8f3be43900ae4c32e6badde7968
Alexandra <atoxtaeva@gmail.com>
2024-03-15 12:24:38 +0300

    variabes are set

--- Lesson_1/Task_1/Program.cs
+++ Lesson_1/Task_1/Program.cs
@@ -1,1 +1,11 @@
-﻿System.Console.WriteLine("Hello world");
+﻿// System.Console.WriteLine("Hello world");
+// - два слеша используются для комментирования срок для того, чтобы оставить доп.информацию или отключить какой-то участок кода.
+int value = 100;
+System.Console.WriteLine(value);
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+System.Console.WriteLine(d);
+System.Console.WriteLine(c);
+System.Console.WriteLine(b);
--- Lesson_1/Task_1/obj/Debug/net8.0/Task_1.AssemblyInfo.cs
+++ Lesson_1/Task_1/obj/Debug/net8.0/Task_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da8787e7873e76767c3cdb8b37ba03153c9b59e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eba4f108f855e201b324d1e2b66fcbe93a52340e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
