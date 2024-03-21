2ab8b548feec978ee30f343b55a8df5198f5470a
Alexandra <atoxtaeva@gmail.com>
2024-03-21 21:29:32 +0300

    add task1 seminar2

---
+++ Seminar_2/task_1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на
+// вход трёхзначное число и удаляет вторую
+// цифру этого числа.
+// Примеры
+// a = 256 => 26
+// a = 891 => 81
+
+
+// ПЕРВЫЙ СПОСОБ - генерация случайного числа
+
+int num = new Random().Next(100,1000);
+System.Console.WriteLine(num);
+
+int num_1 = num % 10;
+// так получаем правое число, т.е. первое
+
+int num_2 = num / 100;
+// так мы получаем левое число, т.е. третье
+
+System.Console.WriteLine(num_2 + "" + num_1); // первый способ склеивания 
+System.Console.WriteLine(num_2 * 10 + num_1); // второй способ математический
+
+int result = num_2 * 10 + num_1;
+System.Console.WriteLine($"{num} -> {result}"); // первый способ вывода результата
+
+int result_2 = Convert.ToInt((num_2 + "" + num_1));
+System.Console.WriteLine($"{num} -> {result_2}"); // второй способ вывода результата
