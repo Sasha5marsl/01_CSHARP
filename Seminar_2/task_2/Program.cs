81984a9467917c4d62ccce1998b5c0e2c3b8fecc
Alexandra <atoxtaeva@gmail.com>
2024-03-21 22:35:41 +0300

    add task2 seminar2

---
+++ Seminar_2/task_2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход
+// трёхзначное число и возводит вторую цифру этого
+// числа в степень, равную третьей цифре.
+// Примеры
+// 487 => 8^7 = 2 097 152
+// 254 => 5^4 = 625
+// 617 => 1
+
+int num = new Random().Next(100,1000);
+System.Console.WriteLine(num);
+
+int num_1 = num % 100; // результат деления по модолю является остаток от деления
+int num_2 = num_1 / 10;
+int num_3 = num_1 % 10;
+
+System.Console.WriteLine(num_1);
+System.Console.WriteLine(num_2);
+System.Console.WriteLine(num_3);
+
+System.Console.WriteLine($"результат:{Math.Pow(num_2,num_3)}");
+
+
+
