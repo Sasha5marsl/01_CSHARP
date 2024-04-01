de623bcd460d79e27bf97790e87edf7f4a8c9a18
Alexandra <atoxtaeva@gmail.com>
2024-04-01 15:16:05 +0300

    add task3 lesson3

---
+++ Lesson_3/task_3/Program.cs
@@ -0,0 +1,28 @@
+﻿// В задаче необходимо заменить четные числа массива на нули, т.е. обнулить четные числа 
+
+void ZeroEvenElements(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+    }
+}
+
+// Напишем функцию для вывода полученных значений из первой функции
+
+void PrintArray(int[] arr)
+{
+    foreach (int e in arr)
+    {
+        System.Console.Write($"{e} ");
+    }
+}
+
+int[] array = { 1, 2, 3, 4, 5 };
+// задаем новый массив для передачи значений в функции
+// вызываем функции 
+ZeroEvenElements(array);
+PrintArray(array);
