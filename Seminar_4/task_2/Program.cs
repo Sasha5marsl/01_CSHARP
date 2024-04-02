08eca51fc206ae1b4a1d76bf726fba6c4df03774
Alexandra <atoxtaeva@gmail.com>
2024-04-02 11:51:34 +0300

    add task2 seminar4

---
+++ Seminar_4/task_2/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задайте массив из N случайных целых чисел (N вводится с
+// клавиатуры).
+// Найдите количество чисел, которые оканчиваются на 1 и
+// делятся нацело на 7.
+// Пример: [1 5 11 21 81 4 0 91 2 3]  => 2
+
+
+System.Console.Write("Введите размерность массива: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = new int[N];
+
+int[] RandomMass(int[] col)
+{
+    for (int i = 0; i < col.Length; i++)
+    {
+        col[i] = new Random().Next(1,100);
+    }
+    return col;
+}
+
+// ПИШЕМ ФУНКЦИЮ, КОТОРАЯ ВЕДЕТ ПОДСЧЕТ ЧИСЕЛ, 
+// ОДНОВРЕМЕННО ЗАКАНЧИВАЮЩИХСЯ НА 1 И КРАТНЫ 7 
+
+int Count(int[] col)
+{
+    int count = 0;
+    for (int i = 0; i < col.Length; i++)
+    {
+        if(col[i] % 10 == 1 && col[i] % 7 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+void PrintMass(int[] coon)
+{
+    foreach (var item in coon)
+    {
+        System.Console.Write($"{item} ");
+    }
+    System.Console.WriteLine();
+}
+
+int[] mas = RandomMass(arr);
+PrintMass(mas);
+int count = Count(mas);
+System.Console.WriteLine($"Количество равно {count}");
