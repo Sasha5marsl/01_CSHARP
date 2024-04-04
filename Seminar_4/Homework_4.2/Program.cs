2cc5c7e2c2f339e720d33e177b719018077ebfb5
Alexandra <atoxtaeva@gmail.com>
2024-04-04 10:26:36 +0300

    add task3 homework4 seminar4

---
+++ Seminar_4/Homework_4.2/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 3: Напишите программу, которая перевернёт одномерный массив 
+// (первый элемент станет последним, второй – предпоследним и т.д.)
+
+System.Console.Write("Введите размерность массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = new int[n]; // создаем сам массив, пока пустой
+
+int[] RandomMass(int[] col)
+{
+    for (int i = 0; i < col.Length; i++)
+    {
+        col[i] = new Random().Next(0,10);
+    }
+    return col;
+}
+
+void PrintMass(int[] coon)
+{
+    foreach (var item in coon)
+    {
+        System.Console.Write($"{item} ");
+    }
+    System.Console.WriteLine();
+}
+
+int[] ReversalMass(int[] port)
+{
+    for (int i = 0; i < port.Length / 2; i++)
+    {
+        int temp = port[i];
+        port[i] = port[port.Length - 1 -i];
+        port[port.Length - 1 -i] = temp;
+    }
+    return port;
+}
+
+int[] mas = RandomMass(arr);
+PrintMass(mas);
+System.Console.WriteLine();
+
+int[] mas2 = ReversalMass(mas);
+PrintMass(mas);
+
+
+
+
+
