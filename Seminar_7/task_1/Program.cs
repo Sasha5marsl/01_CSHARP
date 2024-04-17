9414e14ee860392e389f47029e020b184b930224
Alexandra <atoxtaeva@gmail.com>
2024-04-17 23:58:17 +0300

    add task_1 seminar_7

---
+++ Seminar_7/task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 1. Напишите программу, которая будет принимать 
+// на вход число и возвращать сумму его цифр.
+
+int SumNum(int num)
+{
+    if(num == 0) return 0;
+    int res = num % 10 + SumNum(num / 10);
+    return res;
+}
+
+System.Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine(SumNum(n));
+
+// n = 123 % 10
+// 123 % 10 -> 3
+// 123 / 10 -> 12
+// m = 12 % 10
+// 12 / 10 -> 1
