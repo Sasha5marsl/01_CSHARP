6845d7a96ceab0b2a0f8edf1ce7a15f9a4d06d36
Alexandra <atoxtaeva@gmail.com>
2024-04-18 17:43:45 +0300

    add homework_3 seminar_7

---
+++ Seminar_7/homework_3/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+
+int[] RandomMass(int m)
+{
+    int[] Number = new int[m];
+    for (int i = 0; i < Number.Length; i++)
+    {
+        Number[i] = new Random().Next(1,10);
+    }
+    return Number;
+}
+
+void PrintMas(int[] col)
+{
+    foreach (var item in col)
+    {
+        System.Console.Write($"{item} ");
+    }
+    System.Console.WriteLine();
+}
+
+void PrintMass(int[] coon, int i = 0)
+{
+    if (i == coon.Length) return;
+    PrintMass(coon, i + 1);
+    System.Console.Write($"{coon[i]} ");
+}
+
+System.Console.Write("Введите размерность массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] mas = RandomMass(n);
+PrintMas(mas);
+System.Console.WriteLine();
+PrintMass(mas);
